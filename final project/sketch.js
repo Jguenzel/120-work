cc2e7ec9436c451accde06308c260bcdfcdaeff9
jonas <jonas.guenzel@gmail.com>
2019-04-28 23:09:18 -0600

    update
    fixed sound

--- final project/sketch.js
+++ final project/sketch.js
@@ -1,9 +1,12 @@
 let img;
+let song;
 function preload(){
   img = loadImage("pixelart.png");
+  song = loadSound("media.io_04 Dancing In the Moonlight (2001 Remix).mp3");
 }
 function setup(){
-createCanvas(2000,2000);
+song.play();
+createCanvas(2000,1500);
 }
 
 function draw(){
