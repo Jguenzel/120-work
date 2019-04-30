bb0b82c7b9a88533ea8c2ae618386691f99a8880
jonas <jonas.guenzel@gmail.com>
2019-04-29 22:58:54 -0600

    update

--- final project/sketch.js
+++ final project/sketch.js
@@ -1,8 +1,10 @@
 let img;
 let song;
+let dance;
 function preload(){
   img = loadImage("pixelart.png");
   song = loadSound("media.io_04 Dancing In the Moonlight (2001 Remix).mp3");
+  dance = createImg("dance.gif");
 }
 function setup(){
 song.play();
@@ -11,4 +13,6 @@ createCanvas(2000,1500);
 
 function draw(){
   image(img, 0, 0);
+  dance.size(900, 900);
+  dance.position(500, 600);
 }
